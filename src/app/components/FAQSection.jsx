ea7e04ec2e49ee57fdfbf6e812474a90f42e0e52
Maninder Singh <jack.davidputra2200@gmail.com>
2025-11-13 00:03:14 +0530

    # Fix: FAQ Button Positioning Update

--- src/app/components/FAQSection.jsx
+++ src/app/components/FAQSection.jsx
@@ -88,7 +88,7 @@ export default function FAQSection() {
         {/* CTA Button */}
         <div className="mt-14">
           <a href="/book-demo/">
-            <button className="bg-[#b13200] text-white font-semibold px-10 py-3 rounded-md hover:bg-[#922800] transition-all duration-300">
+            <button className="bg-[#b13200] ml-55 text-white font-semibold px-10 py-3 rounded-md hover:bg-[#922800] transition-all duration-300">
               GET INSTANT ACCESS
             </button>
           </a>
